8bb7f6919633afe6228c220d7cb5e66933b601f5
DanielTymoshenko-ux <draculovich@gmail.com>
2025-10-26 14:08:52 +0100

    Import os module in app.py
    Added import for os module.

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect, session, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from models import db, User, Task
+import os
+
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
