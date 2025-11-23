1ab99297e03bf7d89c202ac91d101fa628afabff
DanielTymoshenko-ux <draculovich@gmail.com>
2025-11-23 22:48:51 +0100

    Modify conftest.py to include system path
    Add system path to the test configuration

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,9 @@
-import pytest
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from app import app as flask_app
+import pytest
 
 @pytest.fixture
 def client():
