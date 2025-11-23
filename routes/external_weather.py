716b2fea4d99b0df4913b8a4ae5f55126b0eddad
DanielTymoshenko-ux <draculovich@gmail.com>
2025-11-23 21:49:05 +0100

    Update weather API route to include '/weather_api'

--- routes/external_weather.py
+++ routes/external_weather.py
@@ -1,9 +1,11 @@
+# routes/external_weather.py
 from flask import Blueprint, request, jsonify, current_app
 from services.weather_client import geocode_city, fetch_weather, WeatherError
 
 weather_bp = Blueprint("weather", __name__)
 
-@weather_bp.route("/", methods=["GET"])
+@weather_bp.route("/weather", methods=["GET"])
+@weather_bp.route("/weather_api", methods=["GET"])
 def weather_api():
     city = request.args.get("city")
     lat = request.args.get("lat")
