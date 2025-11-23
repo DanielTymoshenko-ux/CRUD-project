1a9af03c95dff1d80d39b163dfb626259bb482fa
DanielTymoshenko-ux <draculovich@gmail.com>
2025-11-23 21:40:25 +0100

    Change route from '/' to '/rates_api'

--- routes/external_rates.py
+++ routes/external_rates.py
@@ -1,11 +1,9 @@
-
 from flask import Blueprint, request, jsonify
 from services.currency_client import fetch_rates, CurrencyError
 
-
 rates_bp = Blueprint("rates", __name__)
 
-@rates_bp.route("/", methods=["GET"])
+@rates_bp.route("/rates_api", methods=["GET"])
 def rates_api():
     base = request.args.get("base", "EUR").upper()
     symbols_raw = request.args.get("symbols")
